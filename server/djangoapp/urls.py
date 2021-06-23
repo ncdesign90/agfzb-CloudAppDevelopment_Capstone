194908d9831869b84f67b2ed60b217449b3cc5fc
ncdesign90 <ncdesign90@gmail.com>
2021-06-23 01:16:36 +0000

    User auth

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
     # path for registration
+    path(route='registration/', view=views.registration, name='registration'),
 
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
